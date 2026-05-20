b9507b8d01ae30946c6dfe48f08d363789ffa4b5
Diego <141683307+Diego-Jimy@users.noreply.github.com>
2026-05-19 20:35:48 -0500

    fix: remove duplicated browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from '@context/AuthContext'
 import AppRouter from '@router/AppRouter'
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+      <AppRouter />
     </AuthProvider>
   )
 }
